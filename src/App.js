93dd1498d1e94fcb96a8bc873d4056db00eeb800
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-06-11 09:34:08 -0500

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <Weather />
+      </div>
     </div>
   );
 }
