35ce2c6339aed0424f488dd1efe2e6995c363e0f
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-06-11 10:46:02 -0500

    Fix html to strip of id to work with react

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ export default function Weather() {
         <div className="header">
         <div className="search-bar">
             <form className="Search-form">
-            <input type="search" placeholder="Enter a city" className="form-control search-input" id="searchCity" autocomplete="off" />
+            <input type="search" placeholder="Enter a city" className="form-control search-input" autocomplete="off" />
             <input type="submit" class="btn btn-primary w-100" value="Search"></input>
             </form>
         </div>
@@ -19,18 +19,18 @@ export default function Weather() {
         <div className="row">
           <h1 className="mainCity">Montevideo</h1>
         
-          <div className="mainTemp col-6"><strong id="temperature">12</strong>
+          <div className="mainTemp col-6"><strong>12</strong>
             <span className="units">°C</span>
-            <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" id="weatherImage" />
+            <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" id="weatherImage" alt="partly-cloudy" />
             </div>
  
             <div className="col-6">
             <ul>
-              <b><li id="currentDate">Friday June 11, 2021 at 9:50 AM</li></b>
-              <li id="description">Partly Cloudy</li>
-              <li>Today's High: <strong id="tempHigh">12</strong>°</li>
-              <li>Humidity: <strong id="humidity">55</strong>%</li>
-              <li>Wind Speed: <strong id="windSpeed">13</strong> km/h</li>
+              <b><li>Friday June 11, 2021 at 9:50 AM</li></b>
+              <li>Partly Cloudy</li>
+              <li>Today's High: <strong>12</strong>°</li>
+              <li>Humidity: <strong>55</strong>%</li>
+              <li>Wind Speed: <strong>13</strong> km/h</li>
             </ul>
             </div>
         </div>
