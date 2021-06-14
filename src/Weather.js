1e90e223599f9cadeeca6656604a105f183833d8
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-06-14 14:00:14 -0500

    Add axios and bring over live data from API

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-    return(
-        <div className="wrapper">
-    <div className="Weather">
-        <div className="header">
-        <div className="search-bar">
-            <form className="Search-form row">
-                <div className="col-9">
-            <input type="search" placeholder="Enter a city" className="form-control search-input" autocomplete="off" />
+    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            temperature: response.data.main.temp,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            tempMax: response.data.main.temp_max,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+        })
+        
+        setReady(true);
+    }
+    if (ready) {
+        return(
+            <div className="wrapper">
+        <div className="Weather">
+            <div className="header">
+            <div className="search-bar">
+                <form className="Search-form row">
+                    <div className="col-9">
+                <input type="search" placeholder="Enter a city" className="form-control search-input" autocomplete="off" />
+                </div>
+                <div className="col-3">
+                <input type="submit" className="btn btn-primary w-100" value="Search"></input>
+                </div>
+                </form>
             </div>
-            <div className="col-3">
-            <input type="submit" className="btn btn-primary w-100" value="Search"></input>
             </div>
-            </form>
+    
+    
+        <div className="information">
+            <div className="row">
+              <h1 className="mainCity">{weatherData.city}</h1>
+              <div className="mainTemp col-6">
+                  <div className="clearfix">
+              <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly-cloudy" className="weatherImage" /> {" "}
+                  <span className="displayTemp">{Math.round(weatherData.temperature)}</span>
+                <span className="units">°C</span>
+                </div>
+                </div>
+     
+                <div className="col-6">
+                <ul>
+                  <b><li>Friday June 11 at 9:50 AM</li></b>
+                  <li>{weatherData.description}</li>
+                  <li>Today's High: <strong>{Math.round(weatherData.tempMax)}</strong>°</li>
+                  <li>Humidity: <strong>{weatherData.humidity}</strong>%</li>
+                  <li>Wind Speed: <strong>{weatherData.wind}</strong> km/h</li>
+                </ul>
+                </div>
+            </div>
         </div>
-        </div>
-
-
-    <div className="information">
-        <div className="row">
-          <h1 className="mainCity">Montevideo</h1>
-          <div className="mainTemp col-6">
-              <div className="clearfix">
-          <img src ="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly-cloudy" className="weatherImage" /> {" "}
-              <span className="displayTemp">12</span>
-            <span className="units">°C</span>
-            </div>
-            </div>
- 
-            <div className="col-6">
-            <ul>
-              <b><li>Friday June 11 at 9:50 AM</li></b>
-              <li>Partly Cloudy</li>
-              <li>Today's High: <strong>12</strong>°</li>
-              <li>Humidity: <strong>55</strong>%</li>
-              <li>Wind Speed: <strong>13</strong> km/h</li>
-            </ul>
-            </div>
-        </div>
+    
     </div>
+    <footer><small>
+            <a href="https://github.com/Wantsnewpaint/React-Weather-App" target="_blank" rel="noreferrer">Open-source code</a> by Bridget Young
+            </small></footer>
+        </div>
+        )
+    } else {
+        const apiKey = "dc3dd8fad72c3a037e39c29f90d88da6";
+        let city = "Montevideo"
+        let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        axios.get(apiUrl).then(handleResponse)
 
-</div>
-<footer><small>
-        <a href="https://github.com/Wantsnewpaint/React-Weather-App" target="_blank" rel="noreferrer">Open-source code</a> by Bridget Young
-        </small></footer>
-    </div>
-    )
+        return "Loading..."
+    }
 }
